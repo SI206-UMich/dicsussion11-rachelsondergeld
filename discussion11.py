637abccbc127a770011fb749ab2db99e069c4838
rachelsondergeld <rsondergeld1@gmail.com>
2022-11-15 16:59:37 -0500

    I'm confused lol

--- discussion11.py
+++ discussion11.py
@@ -39,12 +39,18 @@ def create_species_table(cur, conn):
 # TASK 1
 # CREATE TABLE FOR PATIENTS IN DATABASE
 def create_patients_table(cur, conn):
-    pass
+    cur.execute("DROP TABLE IF EXISTS Patients")
+    cur.execute("CREATE TABLE Patients (pet_id INTEGER PRIMARY KEY, name TEXT, species_id INTEGER, \
+         age INTEGER, cuteness INTEGER, aggressiveness INTEGER)")
+
+    conn.commit()
 
 
 # ADD FLUFFLE TO THE TABLE
 def add_fluffle(cur, conn):
-    pass
+    cur.execute("INSERT INTO Patients (pet_id, name, species_id, age, cuteness, aggressiveness)\
+    VALUES (?,?,?,?,?,?)",(0,"Fluffle", "0", "3", "90", "100"))
+    conn.commit()
     
 
 # TASK 2
@@ -59,13 +65,29 @@ def add_pets_from_json(filename, cur, conn):
     json_data = json.loads(file_data)
 
     # THE REST IS UP TO YOU
-    pass
+    # for i in json_data:
+    #     cur.execute("INSERT INTO Patients (pet_id, name, species, age, cuteness, aggressiveness)\
+    #          VALUES(?, ?, ?, ?, ?, ?)", (i, i[0]["name"], i[1]["species"], ..... ))
+    
+    count = 1
+    for i in json_data:
+        pet_id = countname = i["name"]
+        cur.execute("SELECT id from SPecies WHERE title = ?",(i['species'], ))
+        species = int(cur.fetchone()[0])
+        age = int(i["age"])
+        cuteness = int(i["cuteness"])
+        aggressiveness = int(i["aggressiveness"])
+        cur.execute("INSERT INTO Patients (pet_id, name, species, age, cuteness, aggressiveness)\
+              VALUES(?, ?, ?, ?, ?, ?)")
+        count += 1
+
+    conn.commit()
 
 
 # TASK 3
 # CODE TO OUTPUT NON-AGGRESSIVE PETS
 def non_aggressive_pets(aggressiveness, cur, conn):
-    pass
+    lstTup = []
 
 
 
